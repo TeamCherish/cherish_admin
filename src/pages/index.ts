193729b009ba1aaf051ae91c7511d1a5ea264742
soryeongk <soryeongk.kr@gmail.com>
2021-12-17 04:06:48 +0900

    :construction: init: add index-file for pages

---
+++ src/pages/index.ts
@@ -0,0 +1,4 @@
+export { default as Dashboard } from "./Dashboard";
+export { default as Login } from "./Login";
+export { default as Main } from "./Main";
+export { default as User } from "./User";
