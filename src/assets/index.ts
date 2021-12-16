a9cf5092e1be9226eb41973d8f4806bf79615dfc
soryeongk <soryeongk.kr@gmail.com>
2021-12-17 04:06:27 +0900

    :construction: init: add index-file for assets

---
+++ src/assets/index.ts
@@ -0,0 +1,5 @@
+export {default as plant1} from './images/plant1.png';
+export {default as plant2} from './images/plant2.png';
+export {default as plant3} from './images/plant3.png';
+export {default as plant4} from './images/plant4.png';
+export {default as plant5} from './images/plant5.png';
