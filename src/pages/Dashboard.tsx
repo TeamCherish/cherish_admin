870433c816c7d3bda564d82566dcc9ac20f26619
Hwang Juhee <julia0276@naver.com>
2021-12-18 01:45:01 +0900

    :nail_care: style: dashboard페이지에 chart, userlist 추가

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,6 +1,8 @@
-import DashboardSummary from "components/DashboardSummary";
+import DashboardSummary from "../components/dashboard/DashboardSummary";
 import styled from "styled-components";
-
+import { theme } from "styled-tools";
+import ChartWrapper from "components/dashboard/ChartWrapper";
+import UserListWrapper from "components/UserListWrapper";
 import { joinUserIcon, totalUserIcon, totalPlantingIcon } from "../assets";
 
 export default function Dashboard() {
@@ -26,16 +28,42 @@ export default function Dashboard() {
           DataVariation="+7"
         />
       </StSummaryWrapper>
+      <StRowWrapper>
+        <StChartWrapper>
+          <ChartWrapper />
+        </StChartWrapper>
+          <StUserWrapper>
+            <UserListWrapper />
+          </StUserWrapper>
+      </StRowWrapper>
     </StContent>
   );
 }
 
 const StContent = styled.main`
-  width: 151.1rem;
+  width: 150rem;
 `;
 
 const StSummaryWrapper = styled.section`
   display: flex;
   justify-content: space-between;
   width: 100%;
+  margin-bottom: 4.8rem;
+`;
+
+const StRowWrapper = styled.section`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+`;
+
+const StChartWrapper = styled.section`
+  width: 99.2rem;
+  background-color: ${theme("colors.pointWhite")};
+`;
+
+const StUserWrapper = styled.section`
+  width: 47.1rem;
+  height: 95.4rem;
+  background-color: ${theme("colors.pointWhite")};
 `;
