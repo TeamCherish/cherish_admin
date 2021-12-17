264b7aef907a23dcd58c6b18820b286dffebe6ff
SNUPI <47105088+joohaem@users.noreply.github.com>
2021-12-17 11:46:53 +0900

    :bug: fix: http -> https

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const client = axios.create({
-  baseURL: "http://13.209.140.85:8080",
+  baseURL: "https://13.209.140.85:8080",
   headers: {
     "Content-Type": "application/json",
   },
