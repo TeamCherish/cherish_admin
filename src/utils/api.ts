bd333536971e00baae5b0443794f7d581c0cba26
soryeongk <soryeongk.kr@gmail.com>
2022-04-13 01:02:19 +0900

    :tada: feat: 관리자 아이디 검사

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const client = axios.create({
-  baseURL: "http://localhost:5005",
+  baseURL: process.env.REACT_APP_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
